3b3751dfa94e24ccdf98e69df21410c0597303ce
flyingpeng1 <35437929+flyingpeng1@users.noreply.github.com>
2018-10-22 22:34:58 -0400

    idk

--- Mockups/resize.js
+++ Mockups/resize.js
@@ -1,9 +1,14 @@
- $(".panel-left").resizable({
-   handleSelector: ".splitter",
-   resizeHeight: false
- });
-
- $(".panel-top").resizable({
-   handleSelector: ".splitter-horizontal",
-   resizeWidth: false
- });
+	$(".panel-left").resizable({
+	handleSelector: ".splitter",
+	resizeHeight: false
+	});
+	
+	$(".panel-right").resizable({
+	handleSelector: ".splitter2",
+	resizeHeight: false
+	});
+	
+	$(".panel-top").resizable({
+	handleSelector: ".splitter-horizontal",
+	resizeWidth: false
+	});
